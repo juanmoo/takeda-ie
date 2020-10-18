f5f082c7e5e0393988079ba4ea637b0d64ed2476
juanortiz <juanmoo@me.com>
2020-10-18 16:49:08 -0400

    Add counter to dosage/description count names in config file.

--- annotation_tool/config.js
+++ annotation_tool/config.js
@@ -6,36 +6,36 @@ var fieldName = {
     "title": "Study Title",
     "authors": "Authors",
     "study_type": "Study Type",
-    "arm_description": "Arm Description",
-    "arm_dosage": "Arm Dosage",
     "arm_efficacy_metric": "Efficacy Metric",
-    "arm_efficacy_results": "Efficacy Results"
+    "arm_efficacy_results": "Efficacy Results",
+    "arm_description-1": "Arm Description 1",
+    "arm_dosage-1": "Arm Dosage 1",
 }
 var shortName = {
     "title": "Study Title",
     "authors": "Authors",
     "study_type": "Study Type",
-    "arm_description": "Arm Description",
-    "arm_dosage": "Arm Dosage",
     "arm_efficacy_metric": "Efficacy Metric",
-    "arm_efficacy_results": "Efficacy Results"
+    "arm_efficacy_results": "Efficacy Results",
+    "arm_description-1": "Arm Description 1",
+    "arm_dosage-1": "Arm Dosage 1",
 }
 var longDesc = {
     "title": "Study Title",
     "authors": "Authors",
     "study_type": "Study Type",
-    "arm_description": "Arm Description",
-    "arm_dosage": "Arm Dosage",
     "arm_efficacy_metric": "Efficacy Metric",
-    "arm_efficacy_results": "Efficacy Results"
+    "arm_efficacy_results": "Efficacy Results",
+    "arm_description-1": "Arm Description 1",
+    "arm_dosage-1": "Arm Dosage 1",
 }
 var shortcutKey = {
     "title": "Study Title",
     "authors": "Authors",
     "study_type": "Study Type",
-    "arm_description": "Arm Description",
-    "arm_dosage": "Arm Dosage",
     "arm_efficacy_metric": "Efficacy Metric",
-    "arm_efficacy_results": "Efficacy Results"
+    "arm_efficacy_results": "Efficacy Results",
+    "arm_description-1": "Arm Description 1",
+    "arm_dosage-1": "Arm Dosage 1",
 }
 
