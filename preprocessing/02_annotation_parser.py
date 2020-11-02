366de2dcfb730d172e6351b64f91ce54adc3c574
juanortiz <juanmoo@me.com>
2020-11-02 13:53:15 -0500

    Remove dict -> bmes logic from annotation parser file

--- preprocessing/02_annotation_parser.py
+++ preprocessing/02_annotation_parser.py
@@ -10,7 +10,6 @@ relevant_entities = ['title', 'authors', 'study_type', 'arm_efficacy_metric',
 
 # -- (CSV -> Structured Dict) -- #
 
-
 def parse_csv(csv_path, relevant_entities=relevant_entities):
     parsed = dict()
     data = pd.read_csv(csv_path).fillna('')
@@ -67,68 +66,6 @@ def parse_csv(csv_path, relevant_entities=relevant_entities):
 
         parsed[doc_id]['paragraphs'] = paragraphs
     return parsed
-
-
-# -- (Structured Dict -> BMES) -- #
-def make_bmes(data, key_map=bmes_label_map):
-    fnames = data.keys()
-    bmes_list = []
-    for fname in fnames:
-        paragraphs_bmes = []
-        for entry in data[fname]['paragraphs']:
-            text = tokenizer(entry['text']).text
-            bmes_tokens = text.split()
-            bmes_annotations = ['O |'] * len(bmes_tokens)
-
-            lmin = None
-            lmax = None
-            for k in [e for e in entry.keys() if e.endswith('-spans') and 'title' not in e]:
-                key = k[:-6]
-                key = key_map.get(key, key)
-
-                for start, stop in entry[k]:
-                    # For tokens that were annotated multiple times, ranges take precedence over singletons
-                    # bmes_annotations[start] == 'O':
-                    for i in range(start, stop):
-                        if bmes_annotations[i] not in ['O |']:
-                            lmin = start if not lmin else min(lmin, start)
-                            lmax = stop if not lmax else max(lmax, stop)
-                        else:
-                            bmes_annotations[i] = ''
-
-                        if i == start:
-                            if i == stop - 1:
-                                bmes_annotations[start] += f' S-{key} |'
-                            else:
-                                bmes_annotations[i] += f' B-{key} |'
-                        elif i == stop - 1:
-                            bmes_annotations[i] += f' E-{key} |'
-                        else:
-                            bmes_annotations[i] += f' I-{key} |'
-
-            bmes_annotations = list(map(lambda s: s[:-2], bmes_annotations))
-
-            # Merge tags that agree
-            all_merged = True
-            for j, a in enumerate(bmes_annotations):
-                tags = list(set([e.strip() for e in a.split('|')]))
-                if len(tags) == 1:
-                    bmes_annotations[j] = tags[0]
-                else:
-                    all_merged = False
-
-            bmes = list(zip(bmes_tokens, bmes_annotations))
-
-            # Add delimiters in places where disagreeing overlapping annotations
-            # occur in defined range.
-            if lmin and lmax and not all_merged:
-                bmes.insert(lmax, '^-' * 20 + 'STOP' + '-^' * 20)
-                bmes.insert(lmin, 'v-' * 20 + 'START' + '-v' * 20)
-
-            bmes = '\n'.join([' '.join(p) for p in bmes])
-            paragraphs_bmes.append(bmes)
-        bmes_list.append(paragraphs_bmes)
-    return dict(zip(fnames, bmes_list))
 
 
 if __name__ == '__main__':
