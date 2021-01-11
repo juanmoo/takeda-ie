835149f2b627ea5ba1df374756346536b9d12817
Juan M Ortiz <juanmoo@me.com>
2021-01-11 10:57:08 -0500

    RD BIO parsing

--- processing/format.py
+++ processing/format.py
@@ -1,10 +1,20 @@
 '''
 Host logic to transform datasets from one format into another.
 '''
+import json
+import os
+from tqdm import tqdm
+from spacy.lang.en import English
 
 # -- BIO -- #
-def struct_to_bio(struct, task='ner', label_map={}, **kwargs):
+def struct_to_bio(struct, task, label_map={}, **kwargs):
+    if task == 'ner':
+        return struct_to_ner_bio(struct, label_map=label_map, **kwargs)
+    elif task == 'rd':
+        return struct_to_rd_bio(struct, label_map=label_map, **kwargs)
 
+
+def struct_to_ner_bio(struct, label_map={}, **kwargs):
     doc_structs = struct['documents']
     fnames = doc_structs.keys()
     bmes_list = []
@@ -67,11 +77,123 @@ def struct_to_bio(struct, task='ner', label_map={}, **kwargs):
     return dict(zip(fnames, bmes_list))
 
 
+## RD ####
+default_trigger = 'arm_description'
+default_foi = ['arm_efficacy_metric', 'arm_efficacy_results', 'arm_dosage']
+nlp = English()
+nlp.add_pipe(nlp.create_pipe('sentencizer'))
+
+def get_sentences(text):
+    doc = nlp(text)
+    return [sent.string.strip() for sent in doc.sents]
+
+def get_segment(interval, boundaries, window=3):
+    cxt = int((window - 1) / 2)
+    start, end = interval
+    sent_idx_start = 0
+    sent_idx_end = 0
+
+    for i, b in enumerate(boundaries):
+        if start >= b:
+            sent_idx_start = i
+            sent_idx_end = i
+        
+        if end - 1 < boundaries[i+1]:
+            sent_idx_end = i
+            break
+
+    segment_start = boundaries[max(0, sent_idx_start - cxt)]
+    segment_end = boundaries[min(len(boundaries) - 1, sent_idx_end + cxt + 1)]
+    
+    return (segment_start, segment_end)
+
+def process_document(doc_struct, foi=default_foi, trigger=default_trigger, use_sent=False, window=1):
+    data = []
+    trigger_span_key = f'ann-{trigger}-spans'
+    trigger_arm_key = f'ann-{trigger}-arms'
+
+    for par in doc_struct['paragraphs']:
+
+        # Skip paragraphs w/o trigger entities
+        if trigger_span_key not in par or len(par[trigger_span_key]) == 0:
+            continue
+
+        text = par['text']
+        tokens = text.strip().split(' ')
+        sentences = get_sentences(text)
+
+        sent_boundaries = [0]
+        for sent in sentences:
+            sent_boundaries.append(
+                len(sent.strip().split(' ')) + sent_boundaries[-1])
+
+        for span, trigger_arm in zip(par[trigger_span_key], par[trigger_arm_key]):
+
+            tagged_text = []
+            for token in tokens:
+                tagged_text.append([token, 'O'])
+
+            # assign B/I- tags to each token
+            for field in foi:
+                field_span_key = f'ann-{field}-spans'
+                field_arm_key = f'ann-{field}-arms'
+
+                if not field_span_key in par:
+                    # print(f'{field_span_key} not found. Skipping!')
+                    continue
+
+                fval_spans = par[field_span_key]
+                fval_arms = par[field_arm_key]
+
+                for fval_span, fval_arm in zip(fval_spans, fval_arms):
+                    # Add annotation for all non-arm entities and arm entities 
+                    # that match the arm of the trigger word
+                    if fval_arm == -1 or fval_arm == trigger_arm:
+                        start, end = fval_span
+                        tagged_text[start][1] = f'B-{field}'
+                        if end == start + 1:
+                            continue
+                        for i in range(start+1, end):
+                            tagged_text[i][1] = f'I-{field}'
+
+            prod_span_start, prod_span_end = span
+            tagged_text.insert(prod_span_start, ["[P1]", "O"])
+            tagged_text.insert(prod_span_end + 1, ["[P2]", "O"])
+
+            if use_sent:
+                seg_start, seg_end = get_segment(span, sent_boundaries, window=window)
+                tagged_segment = tagged_text[seg_start:(seg_end+2)]
+            else:
+                tagged_segment = tagged_text
+
+            data.append(tagged_segment)
+
+    bio_pars = ['\n'.join(['{}\t{}'.format(t, l) for (t, l) in par_toks]) for par_toks in data]
+    output = '\n\n'.join(bio_pars)
+
+    return output
+
+def struct_to_rd_bio(struct, label_map={}, use_scent=False, **kwargs):
+    doc_structs = struct['documents']
+    output = {}
+
+    for doc_id in doc_structs:
+        output[doc_id] = process_document(doc_structs[doc_id], use_sent=use_scent)
+
+    return output
+
+
+
 # if __name__ == '__main__':
 #     import json
-#     infile = '/Users/juanmoo/Desktop/changed.json'
-#     outfile = '/Users/juanmoo/Desktop/stuff.json'
+
+#     infile = '/data/rsg/nlp/juanmoo1/projects/02_takeda_dev/00_takeda/tmp/changed.json'
+#     out_dir = '/data/rsg/nlp/juanmoo1/projects/02_takeda_dev/00_takeda/tmp/rd_test/'
 
 #     struct = json.load(open(infile, 'r'))
-#     out = struct_to_bio(struct)
-#     json.dump(out, open(outfile, 'w'), indent=4)
+#     rd_out = struct_to_rd_bio(struct)
+
+#     for doc_id in rd_out:
+#         fname = os.path.join(out_dir, doc_id + '.bio')
+#         with open(fname, 'w') as ofile:
+#             ofile.write(rd_out[doc_id])
